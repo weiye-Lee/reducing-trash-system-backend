ef9ce52e5cf1182bff18620232c16097331bd931
李伟业 <weiye_Lee@163.com>
2020-09-20 21:31:30 +0800

    新增Repository

---
+++ src/main/java/com/work/recycle/repository/DTOrderRepository.java
@@ -0,0 +1,6 @@
+package com.work.recycle.repository;
+
+import com.work.recycle.entity.DTOrder;
+
+public interface DTOrderRepository extends BaseRepository<DTOrder,Integer> {
+}
---
+++ src/main/java/com/work/recycle/repository/FCOrderRepository.java
@@ -0,0 +1,6 @@
+package com.work.recycle.repository;
+
+import com.work.recycle.entity.FCOrder;
+
+public interface FCOrderRepository extends BaseRepository<FCOrder,Integer> {
+}
