52fc77e56831a7af747a81cc57e3c964e6fdbec6
李伟业 <weiye_Lee@163.com>
2020-09-26 20:14:17 +0800

    修改完善全局异常处理

--- src/main/java/com/work/recycle/exception/GlobalExceptionHandler.java
+++ src/main/java/com/work/recycle/exception/GlobalExceptionHandler.java
@@ -1,55 +1,56 @@
 package com.work.recycle.exception;
 
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.work.recycle.common.CommonResult;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.work.recycle.result.ResultBody;
-
-
 /**
- *
- * @Title: GlobalExceptionHandler
- * @Description: 全局异常处理
-如果使用@RestControllerAdvice 注解
-则会将返回的数据类型转换成JSON格式
- * @Version:1.0.0
- * @author pancm
- * @date 2018年10月24日
+ * 全局异常处理
+ * Created by macro on 2020/2/27.
  */
-// @ControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-    /**
-     * 处理空指针的异常
-     * @param req
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(value =NullPointerException.class)
+
     @ResponseBody
-    public ResultBody exceptionHandler(HttpServletRequest req, NullPointerException e){
-        logger.error("发生空指针异常！原因是:",e);
-        return ResultBody.error(CommonEnum.BODY_NOT_MATCH);
+    @ExceptionHandler(value = ApiException.class)
+    public CommonResult handle(ApiException e) {
+        if (e.getErrorCode() != null) {
+            return CommonResult.failed(e.getErrorCode());
+        }
+        return CommonResult.failed(e.getMessage());
     }
 
-    /**
-     * 处理其他异常
-     * @param req
-     * @param e
-     * @return
-     */
-    @ExceptionHandler(value =Exception.class)
     @ResponseBody
-    public ResultBody exceptionHandler(HttpServletRequest req, Exception e){
-        logger.error("未知异常！原因是:",e);
-        return ResultBody.error(CommonEnum.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public CommonResult handleValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = null;
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                message = fieldError.getField()+fieldError.getDefaultMessage();
+            }
+        }
+        return CommonResult.validateFailed(message);
     }
 
+    @ResponseBody
+    @ExceptionHandler(value = BindException.class)
+    public CommonResult handleValidException(BindException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = null;
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                message = fieldError.getField()+fieldError.getDefaultMessage();
+            }
+        }
+        return CommonResult.validateFailed(message);
+    }
 }
-
